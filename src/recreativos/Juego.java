e0dc65ed0296a47a44febd986f65000bdd63cd80
Juan Francisco <pjuanzamora@gmail.com>
2020-12-11 14:33:56 +0100

    Esqueleto Juegos Creado

---
+++ src/recreativos/Juego.java
@@ -0,0 +1,54 @@
+package recreativos;
+
+public class Juego {
+	
+	//Atributos de la clase
+	private String nombre;
+	private int record;
+	private boolean multijugador;
+	
+	//Métodos de la clase ------
+	
+	public Juego(String nombre) {
+		this.nombre = nombre;
+		this.record = 0;
+		multijugador = false;
+	}
+	
+	public void ponerRecord(int puntuacion) {
+		record = puntuacion;
+	}
+	
+	public void setRecord(int record) {
+		this.record = record;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public int getRecord() {
+		return record;
+	}
+
+	
+
+	public boolean isMultijugador() {
+		return multijugador;
+	}
+
+	public void setMultijugador(boolean multijugador) {
+		this.multijugador = multijugador;
+	}
+
+	@Override
+	public String toString() {
+		return  nombre + " --- " + record;
+	}
+
+	
+	
+	
+	
+}
---
+++ src/recreativos/Recreativos.java
@@ -0,0 +1,53 @@
+package recreativos;
+
+public class Recreativos {
+
+	public static int pintaMenu() {
+		 
+		//Guardar un nuevo juego
+		//Poner puntuación a un juego
+		//Ver todos los juegos
+		
+	}
+	
+	public static void verTodosJuegos(Juego vJuegos[]) {
+		
+	}
+	
+	public static void guardarJuego(Juego vJuegos[]) {
+		
+	}
+	
+	public static void ponerPuntuacionJuego(Juego vJuegos[]) {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Juego vJuegos[] = new Juego[100];
+		
+		Juego juego1 = new Juego("CiberPunk");
+		Juego juego2 = new Juego("Los Sims");
+		Juego juego3 = new Juego("Fifa");
+		
+		vJuegos[0] = juego1;
+		vJuegos[1] = juego2;
+		vJuegos[2] = juego3;
+		vJuegos[3] = new Juego("Tetris");
+		
+		vJuegos[2].setMultijugador(true);
+		vJuegos[2].ponerRecord(100);
+		
+		for (int i = 0; i < vJuegos.length; i++) {
+			if (vJuegos[i] != null && vJuegos[i].isMultijugador()) {
+				System.out.println(vJuegos[i].toString());
+			}
+			
+		}
+		
+
+	}
+
+}

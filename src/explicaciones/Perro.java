4e6336107cfd65af522db2817a7c81c4c7945684
Juan Francisco <pjuanzamora@gmail.com>
2020-12-14 12:20:12 +0100

    Repaso Objetos java

--- src/explicaciones/Perro.java
+++ src/explicaciones/Perro.java
@@ -1,35 +1,63 @@
 package explicaciones;
 
+import java.util.Random;
+
 public class Perro {
 
 	private String nombre;
+	private int numChip;
 	private int edad;
-	private boolean vacunado;
-
+	
 	public Perro() {
-		this.nombre = "";
-		this.edad = 0;
-		this.vacunado = false;
+		nombre = "";
+		edad =0;
+		numChip = 0;
 	}
 
-	public Perro(String nombre) {
-		this.nombre = nombre;
-		this.edad = 0;
-		this.vacunado = false;
-	}
-
-	public Perro(String nombre, int edad, boolean vacunado) {
+	public Perro(String nombre, int edad) {
 		this.nombre = nombre;
 		this.edad = edad;
-		this.vacunado = vacunado;
+		numChip = 0;
 	}
 
+	public Perro(String nombre, int numChip, int edad) {
+		this.nombre = nombre;
+		this.numChip = numChip;
+		this.edad = edad;
+	}
+
+	
+	public boolean comprobarChip() {
+		if(numChip%2 == 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void poneChip() {
+		if (numChip != 0) {
+			System.out.println("Este perro ya tiene chip");
+		}else {
+			Random r = new Random();
+			this.numChip = r.nextInt(100)+1;
+		}
+	}
+	
 	public String getNombre() {
-		return nombre;
+		return nombre.toUpperCase();
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public int getNumChip() {
+		return numChip;
+	}
+
+	public void setNumChip(int numChip) {
+		this.numChip = numChip;
 	}
 
 	public int getEdad() {
@@ -40,43 +68,14 @@ public class Perro {
 		this.edad = edad;
 	}
 
-	public String isVacunado() {
-		String vacu;
-		if (vacunado==false) {
-			vacu = "No estoy vacunado";
-		}else {
-			vacu = "Estoy vacunado";
-		}
-		return vacu;
-	}
-
-	public void setVacunado(boolean vacunado) {
-		this.vacunado = vacunado;
-	}
-
-	public void cumpleAnos() {
-		if (edad > 15) {
-			System.out.println("Adios chauchau");
-		} else {
-			edad++;
-		}
-	}
-
-	public void ponerVacuna() {
-		if (vacunado == false) {
-			vacunado = true;
-		}else {
-			System.out.println("Ya tengo una vacuna!");
-		}
-	}
-
 	@Override
 	public String toString() {
-		String perro="";
-		
-		perro = nombre + "\n" + edad + "\n" + isVacunado();
-		return perro;
+		return  nombre + " ---- Chip=" + numChip + " --- edad=" + edad;
 	}
+	
+	
+	
+	
 	
 	
 }
--- src/explicaciones/Principal.java
+++ src/explicaciones/Principal.java
@@ -4,12 +4,27 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Gato minusi = new Gato("Minusi", 7, 4);
-		Gato gato1 = new Gato("juan");
-		Gato gato2 = new Gato();
-		Perro pe = new Perro("Leon");
-		System.out.println(pe.toString());
+		Perro p = new Perro();
+		Perro p2 = new Perro("Juan", 5);
+		Perro p3 = new Perro("Juan", 500, 10);
+		Perro pdi = new Perro("León", 7);
 		
+		Perro vPerros[] = new Perro[10];
+		
+		vPerros[0] = p;
+		vPerros[1] = new Perro("Juan", 500, 10);
+		vPerros[2] = new Perro("Juan", 501, 10);
+		vPerros[3] = new Perro("Juan", 502, 10);
+	
+		for (int i = 0; i < vPerros.length; i++) {
+			if (vPerros[i] != null && vPerros[i].comprobarChip()) {
+				System.out.println(vPerros[i].toString());
+			}
+			
+		}
+		
+		
+	
 	}
 
 }

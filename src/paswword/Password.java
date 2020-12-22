c8873266fc2747a4d0c2e957dde5114631899ea3
Juan Francisco <pjuanzamora@gmail.com>
2020-12-22 14:02:02 +0100

    Ejercicio Password finalizado

--- src/paswword/Password.java
+++ src/paswword/Password.java
@@ -35,8 +35,35 @@ public class Password {
 		
 	}
 	
+	//3 mayusculas - 2 minusculas - 6 números
 	public boolean esFuerte() {
+		String letraMin = "abcdefghijklemnñopqrstuvwxyz";
+		String letraMay = letraMin.toUpperCase();
+		String numeros = "0123456789";
+		int contLetraMin=0, contLetraMay=0, contNumeros=0;
 		
+		for (int i = 0; i < longitud; i++) {
+			//Comparo con las letras minúsculas y mayusculas
+			for (int j = 0; j < letraMin.length(); j++) {
+				if (contrasena.substring(i, i+1).equals(letraMin.substring(j,j+1))) {
+					contLetraMin++;
+				}
+				if (contrasena.substring(i, i+1).equals(letraMay.substring(j,j+1))) {
+					contLetraMay++;
+				}
+			}
+			
+			//Comparo con los números
+			for (int j = 0; j < numeros.length(); j++) {
+				if (contrasena.substring(i, i+1).equals(numeros.substring(j,j+1))) {
+					contNumeros++;
+				}
+			}
+		}
+		
+		if ((contLetraMay > 2) && (contLetraMin > 1) && (contNumeros > 5 )) {
+			return true;
+		}
 		
 		return false;
 	}
--- src/paswword/Principal.java
+++ src/paswword/Principal.java
@@ -4,8 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Password p = new Password(5);
+		Password p = new Password(30);
 		System.out.println(p.getContrasena());
+		System.out.println(p.esFuerte());
 	}
 
 }

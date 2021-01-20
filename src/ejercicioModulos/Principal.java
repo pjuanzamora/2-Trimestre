881066bb2a2e44a7761204449dca46eb1a9c7471
Juan Francisco <pjuanzamora@gmail.com>
2021-01-20 12:30:21 +0100

    Versión 2 finalizada

--- src/ejercicioModulos/Principal.java
+++ src/ejercicioModulos/Principal.java
@@ -3,11 +3,23 @@ package ejercicioModulos;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		int suma=0;
+		Modulo vModulos[] = new Modulo[10];
 		Modulo m = new Modulo("Programación", "PRG", 300);
 		
-		System.out.println(m.toString());
-		System.out.println("Numero de horas del ciclo: " + m.getNumHoras());
+		vModulos[0] = m;
+		vModulos[4] = new Modulo("Entornos", "ED", 100);
+		// [m, null, null, null, ed, --.......]
+		
+		for (Modulo mo : vModulos) {
+			if (mo != null) {
+				suma += mo.getNumHoras();
+				System.out.println(mo.toString());
+			}
+		}
+		
+		
+		System.out.println("Numero de horas del ciclo: " + suma);
 
 	}
 

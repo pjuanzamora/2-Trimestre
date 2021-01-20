bbf3029fe18392226544ba08e4e78b0752e0505e
Juan Francisco <pjuanzamora@gmail.com>
2021-01-20 11:13:49 +0100

    Ejercicio Modulos v1.0 finalizado

--- src/ejercicioModulos/Principal.java
+++ src/ejercicioModulos/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		Modulo m = new Modulo("Programación", "PRG", 300);
 		
+		System.out.println(m.toString());
+		System.out.println("Numero de horas del ciclo: " + m.getNumHoras());
 
 	}
 

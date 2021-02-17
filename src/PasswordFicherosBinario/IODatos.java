7b44b76a7b1153dd5835b43cfb0e9245a74535f9
Juan Francisco <pjuanzamora@gmail.com>
2021-02-17 13:13:47 +0100

    Ejercicio Password terminado, empezamos con objetos

--- src/PasswordFicherosBinario/IODatos.java
+++ src/PasswordFicherosBinario/IODatos.java
@@ -1,7 +1,9 @@
 package PasswordFicherosBinario;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -40,6 +42,42 @@ public class IODatos {
 			e1.printStackTrace();
 		}
 		
+	}
+
+	public static Password[] cargarDatos(String rutaFichero) {
+		Password[] vPass = new Password[10];
+		int cont=0;
+		File f = new File(rutaFichero);
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		try (FileInputStream fi = new FileInputStream(f);
+			 DataInputStream leer = new DataInputStream(fi)){
+			
+			while (true) {
+				String usuario = leer.readUTF();
+				int pass = leer.readInt();
+				boolean seguro = leer.readBoolean();
+				vPass[cont] = new Password(usuario, pass, seguro);
+				cont++;
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Fin de lectrua del fichero");
+		}
+		
+		return vPass;
 	}
 	
 }
--- src/PasswordFicherosBinario/Main.java
+++ src/PasswordFicherosBinario/Main.java
@@ -4,13 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Password vPassword[] = new Password[10];
-		vPassword[0] = new Password("Juan", 1, false);
+		//Password vPassword[] = new Password[10];
+		/*vPassword[0] = new Password("Juan", 1, false);
 		vPassword[1] = new Password("Pepe", 2, true);
 		vPassword[2] = new Password("Adelin", 3, true);
-		vPassword[3] = new Password("Jordi", 4, false);
+		vPassword[3] = new Password("Jordi", 4, false);*/
 		
-		IODatos.guardarDatos("pass.dat", vPassword);
+		Password vPassword[] = IODatos.cargarDatos("pass.dat");
+		//Mostarlos por pantalla
+		for (Password p : vPassword) {
+			if (p!=null)
+				System.out.println(p);
+		}
+		
+		//IODatos.guardarDatos("pass.dat", vPassword);
 	}
 
 }
